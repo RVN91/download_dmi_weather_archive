0448f42e0b472780b09b83f756e3aa09ca161a7b
Rasmus Vest Nielsen <rn@civil.aau.dk>
2019-09-11 13:24:25 +0200

    Update download_dmi_weather_archive.py
    Added more pretext.

--- download_dmi_weather_archive.py
+++ download_dmi_weather_archive.py
@@ -1,7 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Reads and stores data from DMI's weather archive:
+Downloads data from DMI's weather archive. In 
+this example, windspeed and direction is 
+downloaded for multiple stations and stored 
+locally in .csv files.
+
 https://www.dmi.dk/vejrarkiv/
+
+To change any parameter look into the 
+request URL:
+
+https://www.dmi.dk/dmidk_obsWS/rest/archive/
+hourly/danmark/wind/Aalborg/2018/Marts/25 
+
+The parameters "wind" and "windir" can be 
+changed to one of the following standard 
+parameters:
+
+"temperature", "precip", "pressure", 
+"humidity", "wind", and "windir".
+
+However, playing around with undocumented 
+common variables, e.g. "radiation" will 
+also return valid data.
 
 If modified or used in any projects, please
 refer to the author.
